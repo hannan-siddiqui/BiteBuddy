c8614f2d5ebc7eb35b01a0db1f7bbd5bda29d123
Hannan Siddiqui <115110479+hannan-siddiqui@users.noreply.github.com>
2024-03-18 04:28:00 +0530

    Add files via upload

---
+++ utils/constant.jsx
@@ -0,0 +1,3 @@
+export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSseGqzFwyPtKygvwgt8HFn_Yt4WVhRp_ljSw&usqp=CAU"
