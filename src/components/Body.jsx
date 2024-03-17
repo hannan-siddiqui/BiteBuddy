594833e185e97d1bb637a210d8ebb2de36575821
Hannan Siddiqui <115110479+hannan-siddiqui@users.noreply.github.com>
2024-03-18 04:25:16 +0530

    add search and filter function

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,79 @@
 import Restaurantcard from "./RestaurantCard";
-import {resdata} from "../utils/mockdata";
+// import {resdata} from "../utils/mockdata";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = ()=>{
+    
+
+    const [reslist, setreslist] = useState([]);
+    const[filteresres, setfilteredres] = useState([]);
+    const[searchtext, setsearchtext] = useState("");
+    
+    useEffect(()=>{
+        fetchdata()
+    }, [])
+
+    const  fetchdata = async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const json = await data.json();
+        // console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+        setreslist(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setfilteredres(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    }
+
+// conditionaling rendering ..........
 
 
-    return (
+    return reslist.length===0?<Shimmer/>:(
         <div className="body">
-            <div className="search">search</div>
+            <div className="filter">
+
+            <div className="search">
+                <input type="text" className="searchbox" value={searchtext}
+                onChange={(e)=>{
+                    setsearchtext(e.target.value);
+                }}
+                
+                />
+                <button
+                onClick = { () => {
+                    const filteredres = reslist.filter((res)=>
+                        res.info.name.toLowerCase().includes(searchtext.toLowerCase())
+                    );
+
+                    setfilteredres(filteredres);
+
+                }}
+
+                >Search</button>
+            </div>
+            
+            <button className="filterbtn"
+
+                onClick={ ()=>{
+                    const topres = reslist.filter(
+                        (res)=>res.info.avgRating>4
+                    )
+                    setfilteredres(topres); 
+                }
+                }
+
+            >
+                Top-Rated Res
+            </button>
+            
+            </div>
 
             <div className="rescon">
 
                 {
-                    resdata.map((restaurant) =>(<Restaurantcard 
-                    key ={restaurant?.info?.id}    
-                    resdata = {restaurant}/>))
+                    filteresres.map((res) =>(<Restaurantcard 
+                    key ={res?.info?.id}    
+                    resdata = {res}/>))
                 }
                 
 
