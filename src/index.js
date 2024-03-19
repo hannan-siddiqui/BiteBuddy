b3b562619077222119dc9c2ec58d15afd93de6e9
Hannan Siddiqui <115110479+hannan-siddiqui@users.noreply.github.com>
2024-03-19 08:03:30 +0530

    add path

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter , RouterProvider, Outlet} from "react-router-dom";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
 
 
 
@@ -9,13 +14,45 @@ const AppLayout = ()=>{
     return(
         <div className="App">
             <Header/>
-            <Body/>
+            <Outlet/>
         </div>
         
     )
 }
 
+const appRouter = createBrowserRouter([
+    {
+        path : "/",
+        element:<AppLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Body/>,
+            },
+            {
+                path : "/about",
+                element:<About/>,
+        
+            },
+            {
+                path : "/contact",
+                element:<Contact/>,
+        
+            },
+            {
+                path:"/restaurants/:resId",
+                element:<RestaurantMenu/>,
+            },
+
+        ],
+        errorElement:<Error/>,
+    },
+
+   
+
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppLayout/>);
+root.render(< RouterProvider router={appRouter}/>);
