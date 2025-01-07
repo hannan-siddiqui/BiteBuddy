d05681d58485bfa79f458ce6106a1280c05203e0
Hannan Siddiqui <115110479+hannan-siddiqui@users.noreply.github.com>
2025-01-07 22:14:58 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import online from "../utils/online";
+import { FaSearch } from "react-icons/fa";
 
 const Body = () => {
   const [reslist, setreslist] = useState([]);
@@ -22,6 +23,7 @@ const Body = () => {
     );
 
     const json = await data.json();
+
     for (let i = 0; i < json?.data?.cards.length; i++) {
       const element = json?.data?.cards[i];
       if (element.card?.card?.gridElements?.infoWithStyle?.restaurants) {
@@ -34,6 +36,7 @@ const Body = () => {
         break;
       }
     }
+    
   };
 
   console.log(reslist);
@@ -47,18 +50,22 @@ const Body = () => {
 
   return (
     <div className="body">
-      <div className="flex  items-center">
-        <div className="m-4 p-4 ">
+      <div className="mt-4 flex justify-center items-center">
+        <div className="flex  items-center  gap-x-2">
+
           <input
             type="text"
-            className="searchbox border border-solid border-black"
+            className="border-[2px] hover:border-white border-stone-600 ml-20 placeholder:text-xl placeholder:text-neutral-400 text-2xl font-semibold  text-neutral-700 w-[400px] 
+            p-2 rounded-xl h-[40px]"
+            placeholder="Search for restaurants and food"
             value={searchtext}
             onChange={(e) => {
               setsearchtext(e.target.value);
             }}
           />
+
           <button
-            className="bg-red-400 mx-3 px-4  rounded-xl "
+            className=" "
             onClick={() => {
               const filteredres = reslist.filter((res) =>
                 res.info.name.toLowerCase().includes(searchtext.toLowerCase())
@@ -67,13 +74,14 @@ const Body = () => {
               setfilteredres(filteredres);
             }}
           >
-            Search
+            <span className="rounded-xl -ml-[56px] flex justify-center rounded-l-0 items-center text-2xl  w-[48px] h-[37px]  p-2 "><span className="font-extrabold text-3xl"><FaSearch/></span> </span>
           </button>
+
         </div>
 
         <div>
           <button
-            className="bg-red-400  px-4  rounded-xl  "
+            className="bg-stone-600 hover:bg-stone-800 text-neutral-200 text-2xl font-bold w-[200px] h-[40px] rounded-xl  "
             onClick={() => {
               const topres = reslist.filter((res) => res.info.avgRating > 4);
               setfilteredres(topres);
@@ -84,7 +92,8 @@ const Body = () => {
         </div>
       </div>
 
-      <div className="flex flex-wrap bg-neutral-700">
+      <div className="  bg-stone-300">
+        <div className="py-16 gap-y-10 gap-x-4 flex flex-wrap justify-center items-center">
         {filteredsres?.map((res) => (
           <Link key={res?.info?.id} to={"/restaurants/" + res?.info?.id}>
             {res?.info?.veg ? (
@@ -94,6 +103,7 @@ const Body = () => {
             )}
           </Link>
         ))}
+        </div>
       </div>
     </div>
   );
